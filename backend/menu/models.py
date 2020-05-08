0bfdaa00739633ce5bd39357a4e21d129ae655ba
Karol Onysko <onielen@gmail.com>
2020-05-08 13:33:22 +0200

    Created Menu and Dish database entity

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=1024, unique=True)
+    description = models.TextField()
+
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=1024, unique=True)
+    description = models.TextField()
+
+    price = models.DecimalField(max_digits=4, decimal_places=2)
+    prepare_time = models.DurationField()
+    is_vegetarian = models.BooleanField()
+
+    modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    menu = models.ForeignKey(
+        Menu,
+        on_delete=models.CASCADE,
+        related_name='dishes'
+    )
